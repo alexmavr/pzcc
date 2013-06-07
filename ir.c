8846764eb3424b41de7ecb9165b4aa31b6795334
Alex Mavrogiannis <nalfemp@gmail.com>
2013-06-07 18:20:22 +0300

    Fixed it. Now integer-to-char casts are correct. The Select operator was pretty handy

--- ir.c
+++ ir.c
@@ -43,18 +43,20 @@ LLVMValueRef cast_compat(Type dest, Type src, LLVMValueRef src_val) {
     } else if ((dest == typeReal) && (src == typeChar)) {
         res = LLVMBuildCast(builder, LLVMUIToFP, src_val, LLVMDoubleType(), "casttmp");
     } else if ((dest == typeChar) && (src == typeInteger)) {
-        /* Our problem: negative numbers are in two's complement form and we need their positive value
-         * We could either: 
-         * a) set the highest order bit to 0 and ignore the rest, claiming this as default behavior for int-to-char conversion
-         * b) check the highest order bit for the sign and if it is 1, 
-         * perform a BuildNeg,BuildSub or BuildXor on the number,
-         * otherwise leave it as is. This requires a branch instruction.
-         * The code below performs (a), but I think we should do (b) instead.
-         *
-         * Maybe postpone it until we start doing branches?
-         */
-        LLVMValueRef imv = LLVMBuildTrunc(builder, src_val, LLVMIntType(7), "trunctmp");
-        res = LLVMBuildZExt(builder, imv, LLVMInt8Type(), "zexttmp");
+        /* Take the Highest order bit (8th bit) of the integer by shifting right 7 times
+         * and truncating to 1 bit.
+         * Using the select operator, use that value to determine the sign.
+         * If that bit is 1, the integer was negative and a Neg operation is performed
+         * If it was 0, the integer was positive.
+         * Finally, truncate the corrent integer to 8 bits */
+
+        LLVMValueRef shift_amm = LLVMConstInt(LLVMInt8Type(), 7, false);
+        LLVMValueRef imv = LLVMBuildLShr(builder, src_val, shift_amm, "lshrtmp"); 
+        imv = LLVMBuildTrunc(builder, imv, LLVMInt1Type(), "trunctmp"); // 1-bit condition
+        LLVMValueRef neg = LLVMBuildNeg(builder, src_val, "negtmp"); // negative branch
+        imv = LLVMBuildSelect(builder, imv, neg, src_val, "selecttmp");
+        res = LLVMBuildTrunc(builder, imv, LLVMInt8Type(), "trunctmp");
+
     } else if ((dest == typeInteger) && (src == typeChar)) {
         res = LLVMBuildZExt(builder, src_val, LLVMInt32Type(), "zexttmp");
     } else {
