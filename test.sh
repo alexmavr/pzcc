e2606adb6abb8b6370e2115ad9b23638e9a56f63
Alex Mavrogiannis <nalfemp@gmail.com>
2013-09-04 18:19:57 +0300

    updated test script, testing malloc failure on string function use

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 ROOT_DIR=/home/afein/Repos/pzcc/
 
 cd $ROOT_DIR
-./pzcc -io tests/linking/2.pzc
-llvm-link tests/linking/2.pzc.imm lib-code/src/pzc.lib -f | llc -O3 -march=x86 -o tests/linking/2.asm
-as --32 tests/linking/2.asm -o tests/linking/2.o
-gcc -m32 -lm tests/linking/2.o -o tests/linking/2
-tests/linking/2
+./pzcc -io tests/linking/$1.pzc
+llvm-link tests/linking/$1.pzc.imm lib-code/src/pzc.lib -f | llc -O3 -march=x86 -o tests/linking/$1.asm
+as --32 tests/linking/$1.asm -o tests/linking/$1.o
+gcc -m32 -lm tests/linking/$1.o -o tests/linking/$1
+tests/linking/$1
